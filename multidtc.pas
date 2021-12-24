31f54e6d16eb024be84648b6d144cf5ba763acc1
dieselnutjob <78536055+dieselnutjob@users.noreply.github.com>
2021-12-24 12:08:31 +0000

    Create multidtc.pas

---
+++ multidtc.pas
@@ -0,0 +1,25 @@
+program multidtc;
+{$mode objfpc}
+uses
+  SysUtils, Process;
+
+var
+  Count : Longint;
+  Message : ansistring;
+  Output : ansistring;
+
+begin
+  if ParamCount>0 then
+  begin
+    for Count:=1 to ParamCount do
+    begin
+      Message := ParamStr(Count);
+      if RunCommand('dtc',['-I','dtb','-O','dts','-o',ParamStr(Count)+'-decompiled.dts',ParamStr(Count)],Output) then
+        Message := Message + ':' + 'Success'
+      else
+        Message := Message + ':' + 'Failed';
+      Writeln(Message);
+    end;
+  end
+  else Writeln('Usage: ./multidtc [dtb files]   eg ./multidtc *.dtb');
+end.
