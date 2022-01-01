a618ada56fb10720bfd67cbb6f42f5a81507ecdc
dieselnutjob <78536055+dieselnutjob@users.noreply.github.com>
2022-01-01 22:15:16 +0000

    Update multidtc.pas
    version2.  Changed output file to .dtb2s

--- multidtc.pas
+++ multidtc.pas
@@ -14,7 +14,7 @@ begin
     for Count:=1 to ParamCount do
     begin
       Message := ParamStr(Count);
-      if RunCommand('dtc',['-I','dtb','-O','dts','-o',ParamStr(Count)+'-decompiled.dts',ParamStr(Count)],Output) then
+      if RunCommand('dtc',['-I','dtb','-O','dts','-o',ParamStr(Count)+'.dtb2s',ParamStr(Count)],Output) then
         Message := Message + ':' + 'Success'
       else
         Message := Message + ':' + 'Failed';
